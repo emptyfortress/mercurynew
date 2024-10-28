92be459d8882859e1ed639e79540c94586db9e77
Volkov Alexander <volkov.525@gmail.com>
2024-10-28 13:57:23 +0300

    add quasar

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/mdi-v7/mdi-v7.css'
+import 'quasar/src/css/index.sass'
+
+import '@/assets/style/main.scss'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +16,17 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(Quasar, {
+	plugins: {}, // import Quasar plugins and add here
+	config: {
+		brand: {
+			primary: '#194a75',
+		},
+		notify: {}, // default set of options for Notify Quasar plugin
+		loading: {}, // default set of options for Loading Quasar plugin
+		loadingBar: {}, // settings for LoadingBar Quasar plugin
+	},
+})
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+	plugins: [
+		vue(),
+		vueDevTools(),
+		quasar({
+			sassVariables: '@/quasar-variables.scss',
+		}),
+	],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	}
 })
