178a3a6ca46deb790d47cf9641e0ce42ad1f2166
Volkov Alexander <volkov.525@gmail.com>
2024-11-06 13:33:56 +0300

    works delay

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -1,37 +1,61 @@
 <script setup lang="ts">
-import { onBeforeUnmount, onMounted } from 'vue';
-import { gsap } from 'gsap'
-import { Flip } from 'gsap/Flip'
-import { useFlip } from '@/stores/flip'
+import { ref, onBeforeUnmount, onMounted } from 'vue';
+// import { gsap } from 'gsap'
+// import { Flip } from 'gsap/Flip'
+// import { useFlip } from '@/stores/flip'
+import { useMotions, useMotion } from '@vueuse/motion'
 
-gsap.registerPlugin(Flip)
+// gsap.registerPlugin(Flip)
 
-const flip = useFlip()
+// const flip = useFlip()
 
-onBeforeUnmount(() => {
-	const elemToFlip = document.querySelector('[data-flip-id]')
-	if (elemToFlip) {
-		let tmp = Flip.getState(elemToFlip)
-		flip.setLastState(tmp)
-	}
-})
+// onBeforeUnmount(() => {
+// 	const elemToFlip = document.querySelector('[data-flip-id]')
+// 	if (elemToFlip) {
+// 		let tmp = Flip.getState(elemToFlip)
+// 		flip.setLastState(tmp)
+// 	}
+// })
+//
+// onMounted(() => {
+// 	const elemToFlip = document.querySelector('[data-flip-id]')
+// 	if (!!elemToFlip && !!flip.lastState) {
+// 		Flip.from(flip.lastState, {
+// 			targets: elemToFlip,
+// 			duration: .3,
+// 			toggleClass: 'test'
+// 			// other Flip properties
+// 		});
+// 	}
+// 	flip.setLastState(null)
+// })
+//
+//
 
-onMounted(() => {
-	const elemToFlip = document.querySelector('[data-flip-id]')
-	if (!!elemToFlip && !!flip.lastState) {
-		Flip.from(flip.lastState, {
-			targets: elemToFlip,
-			duration: .3,
-			toggleClass: 'test'
-			// other Flip properties
-		});
-	}
-	flip.setLastState(null)
-})
+const cube = ref<HTMLElement>()
+const box = ref<HTMLElement>()
+
+const cubeVariants = {
+	initial: { scale: 1 },
+	enter: { scale: 0.5, transition: { delay: 1000, onComplete: () => (variant.value = 'custom') } },
+	custom: { scale: 2, transition: { delay: 1000, onComplete: () => (variant.value = 'initial') } }
+}
+
+const { variant } = useMotion(cube, cubeVariants)
+
+const action = async () => {
+	// await cubeAnim('set1')
+	// await cubeAnim('enter')
+}
 </script>
 
 <template lang="pug">
-q-page
+q-page(padding)
+	q-btn(flat icon="mdi-close" color="primary" label="Отмена" @click="action") 
+	.row.q-gutter-x-sm
+		.cube(ref='cube')
+		.box(ref="box")
+
 	RouterLink.link(data-flip-id="img" to="/project")
 </template>
 
@@ -44,5 +68,17 @@ q-page
 	background: pink;
 	width: 124px;
 	height: 124px;
+}
+
+.cube {
+	width: 300px;
+	height: 200px;
+	background: #fff;
+}
+
+.box {
+	width: 100px;
+	height: 100px;
+	background: red;
 }
 </style>
