a1385689a29a423420e5d2f4b5dc0037e54afdf2
Volkov Alexander <volkov.525@gmail.com>
2024-11-25 11:24:11 +0300

    before move on

--- src/components/AppGrid1.vue
+++ src/components/AppGrid1.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { ref, onMounted, nextTick, reactive, onBeforeUnmount, } from 'vue'
-import { onBeforeRouteLeave, useRouter, } from 'vue-router'
+import { ref, onMounted, nextTick, reactive, onBeforeUnmount } from 'vue'
+import { onBeforeRouteLeave, useRouter } from 'vue-router'
 import { gsap } from 'gsap'
 import { Flip } from 'gsap/Flip'
 import { useFlip } from '@/stores/flip'
@@ -15,25 +15,13 @@ const router = useRouter()
 
 gsap.registerPlugin(Flip)
 
-// onMounted(() => {
-// 	gsap.fromTo('.item', {
-// 		y: 100,
-// 		opacity: 0,
-// 	}, {
-// 		y: 0,
-// 		opacity: 1,
-// 		stagger: 0.2,
-// 		delay: .5
-// 	})
-// })
-
 const expanded = ref<boolean>(false)
 
-const calcClass = ((item: any) => {
+const calcClass = (item: any) => {
 	if (expanded.value == true && item.expand == true) return 'active'
 	if (expanded.value == true && item.expand == false) return 'inactive'
 	else return ''
-})
+}
 
 const expand = (item: any) => {
 	const state = Flip.getState('.item')
@@ -44,8 +32,14 @@ const expand = (item: any) => {
 			duration: 0.4,
 			ease: 'power3.inOut',
 			absolute: true,
-			onEnter: (elements) => gsap.fromTo(elements, { opacity: 0, }, { opacity: 1, duration: 0.6, ease: "linear", delay: .2 }),
-			onLeave: (elements) => gsap.fromTo(elements, { opacity: 1, }, { opacity: 0, duration: 0.2, ease: "linear", }),
+			onEnter: (elements) =>
+				gsap.fromTo(
+					elements,
+					{ opacity: 0 },
+					{ opacity: 1, duration: 0.6, ease: 'linear', delay: 0.2 }
+				),
+			onLeave: (elements) =>
+				gsap.fromTo(elements, { opacity: 1 }, { opacity: 0, duration: 0.2, ease: 'linear' }),
 		})
 	})
 	// if (expanded.value) {
@@ -71,9 +65,9 @@ const expand = (item: any) => {
 // })
 const list = ref(myapps.apps)
 
-const remove = ((n: number) => {
+const remove = (n: number) => {
 	console.log(111)
-})
+}
 </script>
 
 <template lang="pug">
@@ -88,28 +82,26 @@ const remove = ((n: number) => {
 		:class="calcClass(item)"
 		) {{ item.label }}
 
-	// template(#item="{ element }")
-		.item {{ element.label }}
 
 // .item(
-	v-for="(item, index) in myapps.apps"
-	:key="item.id"
-	v-motion
-	:initial="{ y: 100, opacity: 0 }"
-	:enter='{ y: 0, opacity: 1, transition: { delay: 100 + (100 * index) } }'
-	@click='expand(item)'
-	:class="calcClass(item)")
-
-	.hd(@click="remove(index)") {{ item.label }}
-	.bl
-		div(v-if='expanded')
-			.desc Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, libero. Impedit, distinctio sed at optio exercitationem quos culpa? Atque vitae aspernatur possimus praesentium culpa id eum! Velit dolores eos aliquam?
-			.desc Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, libero. Impedit, distinctio sed at optio exercitationem quos culpa? Atque vitae aspernatur possimus praesentium culpa id eum! Velit dolores eos aliquam?
-		q-card-actions(v-if='expanded')
-			RouterLink.link(@click.stop='' to='/assistent' data-flip-id='img') lkajslk
-			q-space
-			q-btn(flat color="primary" label="Отмена" @click="") 
-			q-btn(unelevated color="primary" label="Настройки" @click.stop="") 
+// 	v-for="(item, index) in myapps.apps"
+// 	:key="item.id"
+// 	v-motion
+// 	:initial="{ y: 100, opacity: 0 }"
+// 	:enter='{ y: 0, opacity: 1, transition: { delay: 100 + (100 * index) } }'
+// 	@click='expand(item)'
+// 	:class="calcClass(item)")
+//
+// 	.hd(@click="remove(index)") {{ item.label }}
+// 	.bl
+// 		div(v-if='expanded')
+// 			.desc Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, libero. Impedit, distinctio sed at optio exercitationem quos culpa? Atque vitae aspernatur possimus praesentium culpa id eum! Velit dolores eos aliquam?
+// 			.desc Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, libero. Impedit, distinctio sed at optio exercitationem quos culpa? Atque vitae aspernatur possimus praesentium culpa id eum! Velit dolores eos aliquam?
+// 		q-card-actions(v-if='expanded')
+// 			RouterLink.link(@click.stop='' to='/assistent' data-flip-id='img') lkajslk
+// 			q-space
+// 			q-btn(flat color="primary" label="Отмена" @click="") 
+// 			q-btn(unelevated color="primary" label="Настройки" @click.stop="") 
 
 </template>
 
@@ -138,14 +130,14 @@ const remove = ((n: number) => {
 
 .desc {
 	margin-top: 1rem;
-	font-size: .9rem;
+	font-size: 0.9rem;
 }
 
 .item {
 	width: 200px;
 	height: 200px;
 	background: #fff;
-	border-radius: .5rem;
+	border-radius: 0.5rem;
 	cursor: pointer;
 	padding: 1rem;
 
@@ -182,7 +174,7 @@ const remove = ((n: number) => {
 	border: 1px solid #ccc;
 	background: #fff;
 	box-shadow: 2px 2px 6px rgba($color: #000000, $alpha: 0.2);
-	border-radius: .5rem;
+	border-radius: 0.5rem;
 	display: none;
 
 	&.active {
@@ -201,6 +193,7 @@ const remove = ((n: number) => {
 
 .list {
 	display: flex;
-	gap: .5rem;
+	gap: 0.5rem;
+	flex-wrap: wrap;
 }
 </style>
--- src/views/TestPage1.vue
+++ src/views/TestPage1.vue
@@ -1,100 +1,78 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-import { Container, Draggable } from "vue3-smooth-dnd"
-import { applyDrag } from '@/utils/utils'
-import { useKeyModifier } from '@vueuse/core'
-
-
-const cube = ref<HTMLElement>()
+import { ref, computed } from "vue";
+import { Container, Draggable } from "vue3-smooth-dnd";
+import { applyDrag } from "@/utils/utils";
+import { useKeyModifier } from "@vueuse/core";
 
 const tapes = ref([
-	{ id: 0 },
-	{ id: 1 },
-	{ id: 2 },
-	{ id: 3 },
-])
-const tap = ref([
-])
+  { id: 0, group: false },
+  { id: 1, group: false },
+  { id: 2, group: false },
+  { id: 3, group: false },
+]);
 
-const onDrop = ((dropResult: number) => {
-	console.log('fuck')
-	tapes.value = applyDrag(tapes.value, dropResult)
-})
+const tap = ref([]);
 
-// const onDrop1 = (() => {
-// 	console.log(111)
-// 	tap.value = applyDrag(tap.value, dropResult)
-// })
+const onDrop = (dropResult: number) => {
+  console.log("fuck");
+  tapes.value = applyDrag(tapes.value, dropResult);
+};
 
-const shift = useKeyModifier('Shift')
+const shift = useKeyModifier("Shift");
 
-const typ = computed(() => {
-	return shift.value ? 1 : 0
-})
+const type = computed(() => {
+  return shift.value ? 1 : 0;
+});
 
-const drop = ref(false)
+const acceptDrop = () => {
+  return !shift.value;
+};
 
-const drp = (() => {
-	return !shift.value
-})
+const hoverItem = ref();
+const draggingItem = ref();
 
-const over = ref(false)
+const onDragEnter = (index: number) => {
+  hoverItem.value = index;
+};
+const onDragLeave = () => {
+  hoverItem.value = null;
+};
 
-const hoverItem = ref()
-const draggingItem = ref()
+const calcOver = (index: number) => {
+  if (hoverItem.value == index && index !== draggingItem.value) return "green";
+  return "";
+};
 
-const onDragEnter = ((ind: number) => {
-	hoverItem.value = ind
-})
-const onDragLeave = (() => {
-	hoverItem.value = null
-})
+const onDragStart = (n: number) => {
+  draggingItem.value = n;
+};
 
-const calcOver = ((ind: number) => {
-	if (hoverItem.value == ind && ind !== draggingItem.value) return 'red'
-	return ''
-})
+const onDrop1 = () => {
+  let item = tapes.value[hoverItem.value];
+  item.group = true;
+  console.log(item);
 
-const onDragStart = ((n: number) => {
-	draggingItem.value = n
-})
-const onDrp = (() => {
-	console.log('drop')
-	onDragLeave()
-	tapes.value.splice(draggingItem.value, 1)
-	draggingItem.value = null
-})
+  onDragLeave();
+  tapes.value.splice(draggingItem.value, 1);
+  draggingItem.value = null;
+};
 </script>
 
 <template lang="pug">
 q-page(padding)
-	q-checkbox(v-model="drop")
-	ul
-		// .cube(ref='cube' @dragover.prevent="onDragEnter" @dragenter.prevent @dragleave="onDragLeave" :class="{ red: over }" @drop='onDrp' )
-
-		// .fuck(v-for="(item, index) in tapes"
-		// 	:key="item.id"
-		// 	:draggable='true'
-		// 	@dragstart='onDragStart(index)'
-		// 	@dragover.prevent="onDragEnter(index)"
-		// 	@dragenter.prevent
-		// 	@dragleave="onDragLeave"
-		// 	@drop='onDrp'
-		// 	:class='calcOver(index)'
-		// 	) {{ item.id }}
 
 	ul
-		Container(v-if='typ == 0' @drop="onDrop" orientation='horizontal' :should-accept-drop='drp' group-name='column')
+		Container(v-if='type == 0' @drop="onDrop" orientation='horizontal' :should-accept-drop='acceptDrop' group-name='column')
 			Draggable(v-for="(item, index) in tapes" :key="item.id")
-				.fuck() {{ item.id }}
-		.fuck(v-if='typ == 1' v-for="(item, index) in tapes"
+				.fuck(:class="{ group: item.group }") {{ item.id }}
+		.fuck(v-if='type == 1' v-for="(item, index) in tapes"
 			:key="item.id"
 			:draggable='true'
 			@dragstart='onDragStart(index)'
 			@dragover.prevent="onDragEnter(index)"
 			@dragenter.prevent
 			@dragleave="onDragLeave"
-			@drop='onDrp'
+			@drop='onDrop1'
 			:class='calcOver(index)'
 			) {{ item.id }}
 
@@ -105,58 +83,63 @@ q-page(padding)
 
 <style scoped lang="scss">
 q-page {
-	position: relative;
-}
-
-.out {
-	width: 100%;
-	height: 100%;
-	background: pink;
+  // position: relative;
 }
 
 ul {
-	display: flex;
-	flex-wrap: wrap;
+  display: flex;
+  flex-wrap: wrap;
 }
 
 .target {
-	position: fixed;
-	left: 50%;
-	bottom: 1rem;
-	width: 200px;
-	height: 100px;
-	background: #ccc;
-	border: 2px dashed black;
+  position: fixed;
+  left: 50%;
+  bottom: 1rem;
+  width: 200px;
+  height: 100px;
+  background: #ccc;
+  border: 2px dashed black;
 }
 
 .fuck {
-	width: 100px;
-	height: 100px;
-	background: #fff;
-	margin: .25rem;
+  width: 100px;
+  height: 100px;
+  background: #fff;
+  margin: 0.25rem;
+  position: relative;
 
-	&.red {
-		background: pink;
-	}
+  &.group {
+    box-shadow:
+      2px 2px 3px rgba($color: #000000, $alpha: 0.2),
+      -1px -1px 2px rgba($color: #000000, $alpha: 0.2);
+
+    &:before {
+      content: "";
+      display: block;
+      width: 100px;
+      height: 100px;
+      background: #fff;
+      position: absolute;
+      top: 0px;
+      left: 0px;
+      z-index: -2;
+      transform: rotate(5deg);
+      box-shadow: 1px 1px 5px rgba($color: #000000, $alpha: 0.2);
+    }
+  }
+
+  &.green {
+    background: #a8d7a8;
+  }
 }
 
 .link {
-	position: absolute;
-	bottom: 2rem;
-	right: 2rem;
-	background: #fff;
-	background: pink;
-	width: 124px;
-	height: 124px;
-}
-
-.cube {
-	width: 300px;
-	height: 200px;
-	background: yellow;
-
-	&.red {
-		background: pink;
-	}
+  position: absolute;
+  bottom: 2rem;
+  right: 2rem;
+  background: #fff;
+  background: pink;
+  width: 124px;
+  height: 124px;
 }
 </style>
