fec3a187a2a38d6abcdf7afdf7e70e6ad281b985
Volkov Alexander <volkov.525@gmail.com>
2024-11-27 12:23:22 +0300

    change primary

--- src/views/Process.vue
+++ src/views/Process.vue
@@ -7,12 +7,13 @@ import VueDraggableResizable from 'vue-draggable-resizable'
 const app = useStorage('app', localStorage)
 
 const blocks = ref([
-	{ id: 0, selected: false },
 	{ id: 1, selected: false },
 	{ id: 2, selected: false },
+	{ id: 3, selected: false },
 ])
 
 const editor = ref<HTMLElement>()
+const prop = ref<HTMLElement>()
 
 const { apply: editorAnim, stop } = useMotion(editor, {
 	enter: {
@@ -35,8 +36,29 @@ const { apply: editorAnim, stop } = useMotion(editor, {
 		},
 	},
 	start: { width: '90%', x: '0%', transition: { stiffness: 200, damping: 20 } },
-	shrink: { width: '60%', x: '-30%', transition: { stiffness: 200, damping: 20 } },
+	shrink: { width: '70%', x: '-20%', transition: { stiffness: 200, damping: 20 } },
 	move: { x: -300, transition: { stiffness: 200, damping: 20 } },
+})
+const { apply: propAnim, stop1 } = useMotion(prop, {
+	initial: {
+		opacity: 0,
+		scaleX: '0%',
+	},
+	enter: {
+		opacity: 1,
+		scaleX: '100%',
+		transition: { stiffness: 200, damping: 20 },
+	},
+	start: {
+		opacity: 1,
+		scaleX: '100%',
+		transition: { stiffness: 200, damping: 20 },
+	},
+	exit: {
+		opacity: 0,
+		scaleX: '0%',
+		transition: { stiffness: 200, damping: 20 },
+	},
 })
 
 const selection = ref<number | null>(null)
@@ -77,8 +99,10 @@ q-page(padding)
 				@click='select(item.id)'
 				) {{ item.id }}
 		.footer footer {{ selection }}
+		.prop(ref='prop' v-if='selection')
+			.text Свойства
+			.text Этап {{ selection }}
 
-	// .test()
 	vue-draggable-resizable()
 		div laдофыдво
 
@@ -87,30 +111,39 @@ q-page(padding)
 <style scoped lang="scss">
 .q-page {
 	display: flex;
-	// align-items: start;
 	justify-content: center;
 	position: relative;
 }
 .editor {
-	// width: 1280px;
-	// height: 600px;
+	width: 90%;
 	height: calc(100vh - 120px);
 	background: #fff;
 	padding: 0.5rem;
 	border-radius: 0.4rem;
+	border: 1px solid #ccc;
 	display: flex;
 	justify-content: space-between;
 	flex-direction: column;
 	align-items: center;
 	box-shadow: 0 11px 20px 0 rgba(0, 0, 0, 0.2);
-	overflow: hidden;
+	// overflow: hidden;
 	transform-origin: bottom right;
+	position: relative;
 }
-// .inner {
-// 	width: 300px;
-// 	overflow: hidden;
-// 	background: pink;
-// }
+.prop {
+	width: 40%;
+	height: calc(100vh - 120px);
+	background: #fff;
+	box-shadow: 0 11px 20px 0 rgba(0, 0, 0, 0.2);
+	border-radius: 0.4rem;
+	padding: 0.5rem;
+	border: 1px solid #ccc;
+	// overflow: hidden;
+	position: absolute;
+	top: 0;
+	right: -41%;
+	z-index: -3;
+}
 .text {
 	font-size: 1.2rem;
 }
