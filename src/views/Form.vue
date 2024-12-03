0b2f9c172ee0efb5e6d3f41f98bc6303cd30d1d1
Volkov Alexander <volkov.525@gmail.com>
2024-12-03 10:45:43 +0300

    fix floating bug

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,7 +1,62 @@
 <script setup lang="ts">
+import { ref, computed } from 'vue'
 import PlusButton1 from '@/components/PlusButton1.vue'
 import LibButton from '@/components/LibButton.vue'
-import { editor, startLeft, startRight, stopRight, stopLeft } from '@/composable/panel3'
+import { usePanels } from '@/stores/panels'
+import { useMotion } from '@vueuse/motion'
+
+const panels = usePanels()
+const editor = ref<HTMLElement>()
+
+const full = { width: 1500, x: 0 }
+const leftStart = { width: 1150, x: 175 }
+const rightStart = { width: 1150, x: -175 }
+const allStart = { width: 800, x: 0 }
+
+const calcStart = computed(() => {
+	if (panels.left && panels.right) return allStart
+	if (!panels.left && !panels.right) return full
+	if (panels.left) return leftStart
+	if (panels.right) return rightStart
+})
+
+const { apply: editorAnim, stop } = useMotion(editor, {
+	enter: calcStart.value,
+	start: { width: 1500, x: 0, transition: { stiffness: 200, damping: 20 } },
+	shrinkRight: { width: 1150, x: -175, transition: { stiffness: 200, damping: 20 } },
+	shrinkLeft: { width: 1150, x: 175, transition: { stiffness: 200, damping: 20 } },
+	shrinkAll: { width: 800, x: 0, transition: { stiffness: 200, damping: 20 } },
+})
+
+const startRight = async () => {
+	panels.setRight(true)
+	if (panels.left && panels.right) {
+		await editorAnim('shrinkAll')
+	} else await editorAnim('shrinkRight')
+	stop()
+}
+
+const startLeft = async () => {
+	panels.setLeft(true)
+	if (panels.left && panels.right) {
+		await editorAnim('shrinkAll')
+	} else await editorAnim('shrinkLeft')
+	stop()
+}
+
+const stopRight = async () => {
+	setTimeout(() => {
+		panels.left ? editorAnim('shrinkLeft') : editorAnim('start')
+	}, 400)
+	stop()
+}
+
+const stopLeft = async () => {
+	setTimeout(() => {
+		panels.right ? editorAnim('shrinkRight') : editorAnim('start')
+	}, 400)
+	stop()
+}
 </script>
 
 <template lang="pug">
--- src/views/Process.vue
+++ src/views/Process.vue
@@ -1,8 +1,41 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
 import PlusButton from '@/components/PlusButton.vue'
-import { editor1, startRight0, stopRight0 } from '@/composable/panel0'
+// import { editor1, startRight0, stopRight0 } from '@/composable/panel0'
+import { useMotion } from '@vueuse/motion'
+import { usePanels } from '@/stores/panels'
 
+const editor1 = ref<HTMLElement>()
+
+const panels = usePanels()
+
+const full = { width: 1500, x: 0 }
+const rightStart = { width: 1150, x: -175 }
+
+const calcStart = computed(() => {
+	if (!panels.right0) return full
+	if (panels.right0) return rightStart
+})
+
+const { apply: editorAnim, stop } = useMotion(editor1, {
+	initial: calcStart.value,
+	enter: calcStart.value,
+	start: { width: 1500, x: 0, transition: { stiffness: 200, damping: 20 } },
+	shrinkRight: { width: 1150, x: -175, transition: { stiffness: 200, damping: 20 } },
+})
+
+const startRight0 = async () => {
+	panels.setRight0(true)
+	await editorAnim('shrinkRight')
+	stop()
+}
+
+const stopRight0 = async () => {
+	setTimeout(() => {
+		editorAnim('start')
+	}, 400)
+	stop()
+}
 const blocks = ref([
 	{ id: 1, selected: false },
 	{ id: 2, selected: false },
