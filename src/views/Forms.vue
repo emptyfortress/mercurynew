a14234bb97df11be2ac7412dac59d2df0654b366
Volkov Alexander <volkov.525@gmail.com>
2024-12-11 13:59:47 +0300

    fix error

--- src/views/Forms.vue
+++ src/views/Forms.vue
@@ -46,12 +46,13 @@ const expand = (item: any) => {
 	})
 }
 
-const calcClass = (item: App) => {
-	if (expanded.value == true && item.expand == true) return 'active'
-	if (expanded.value == true && item.expand == false) return 'inactive'
-	if (item.group == true) return 'group'
-	else return ''
-}
+// const calcClass = (item: App) => {
+// 	if (expanded.value == true && item.expand == true) return 'active'
+// 	if (expanded.value == true && item.expand == false) return 'inactive'
+// 	if (item.group == true) return 'group'
+// 	else return ''
+// }
+
 </script>
 
 <template lang="pug">
@@ -66,13 +67,12 @@ q-page(padding)
 					:initial="{ y: 100, opacity: 0 }"
 					:enter='{ y: 0, opacity: 1, transition: { delay: 300 + (100 * index) } }'
 					@click='expand(item)'
-					:class="calcClass(item)"
 					)
 					svg(xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24")
 						path(fill="currentColor" d="M12.308 16.25q-.343 0-.576-.232t-.232-.576V8.558q0-.343.232-.576t.576-.232h6.884q.344 0 .576.232t.232.576v6.884q0 .344-.232.576t-.576.232zm.192-1H19v-6.5h-6.5zM4.5 20q-.213 0-.356-.144T4 19.499t.144-.356T4.5 19h15q.213 0 .356.144t.144.357t-.144.356T19.5 20zm0-3.75q-.213 0-.356-.144T4 15.749t.144-.356t.356-.143h4.116q.212 0 .356.144t.144.357t-.144.356t-.356.143zm0-3.75q-.213 0-.356-.144T4 11.999t.144-.356t.356-.143h4.116q.212 0 .356.144t.144.357t-.144.356t-.356.143zm0-3.75q-.213 0-.356-.144T4 8.249t.144-.356t.356-.143h4.116q.212 0 .356.144t.144.357t-.144.356t-.356.143zM4.5 5q-.213 0-.356-.144T4 4.499t.144-.356T4.5 4h15q.213 0 .356.144t.144.357t-.144.356T19.5 5z")
 				div {{ item.label }}
 
-		q-btn.q-ml-xl(round icon="mdi-plus" color="primary" @click="action") 
+		q-btn.q-ml-xl(round icon="mdi-plus" color="primary" @click="") 
 </template>
 
 <style scoped lang="scss">
