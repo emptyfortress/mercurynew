d5ed1c9dab8470e0bd683201e3f9b0e7a1d71e9b
Volkov Alexander <volkov.525@gmail.com>
2024-12-18 17:37:52 +0300

    enter with gsap

--- src/views/Tmp.vue
+++ src/views/Tmp.vue
@@ -1,38 +1,51 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useMotion } from '@vueuse/motion'
+import { ref, nextTick } from 'vue'
+import { gsap } from 'gsap'
+import { Flip } from 'gsap/Flip'
+// import { useMotion } from '@vueuse/motion'
 
-const item = ref()
-const item1 = ref()
+gsap.registerPlugin(Flip)
 
-const { apply } = useMotion(item, {
-	initial: { x: 0, y: 0, rotate: '0deg', opacity: 0 },
-	// enter: { x: 0, y: 0, opacity: 1 },
-	jump: { x: 200, rotate: '3deg', opacity: 1, },
-})
+// const item = ref()
+// const item1 = ref()
 
-const { apply: newItem } = useMotion(item1, {
-	initial: { x: 0, y: 0, rotate: '0deg', opacity: 0 },
-	// enter: { x: 0, y: 0, opacity: 1 },
-	jump: { x: 400, rotate: '-3deg', opacity: 1 },
-})
 
 const expand = () => {
 	console.log(111)
-	expanded.value = true
-	apply('jump')
-	newItem('jump')
+	const state = Flip.getState('.it')
+	expanded.value = !expanded.value
+	nextTick(() => {
+		Flip.from(state, {
+			duration: .4,
+			targets: '.it',
+			absolute: true,
+			absoluteOnLeave: true,
+			ease: "elastic.out(.7, 0.9)",
+			// ease: 'power3.inOut',
+		})
+	})
 }
 const expanded = ref(false)
+
+const arr = [
+	{ id: 0 },
+	{ id: 1 },
+	{ id: 2 },
+]
 </script>
 
 <template lang="pug">
 q-page(padding)
 	.list
 		.it
-		.it( @click="expand")
-			.child(v-if='expanded' ref="item")
-			.child(v-if='expanded' ref="item1")
+		.it( @click="expand" :class="{ active: expanded }")
+			.child(v-if='expanded'
+				v-for="(item, index) in arr" :key='item.id'
+				v-motion
+				:initial="{ scale: .3, opacity: 0 }"
+				:enter="{ scale: 1, opacity: 1, transition: { delay: 200 + 100 * index } }"
+				)
+		.it
 </template>
 
 <style scoped lang="scss">
@@ -46,15 +59,37 @@ q-page(padding)
 	height: 150px;
 	background: #fff;
 	position: relative;
+
+	&.active {
+		position: fixed;
+		height: 70vh;
+		width: 900px;
+		margin: 0 auto;
+		left: 0;
+		right: 0;
+		border: 1px solid #ccc;
+		box-shadow: 2px 2px 6px rgba($color: #000000, $alpha: 0.2);
+
+		display: flex;
+		justify-content: center;
+		gap: 1rem;
+		background: #ccc;
+		padding: 1rem;
+		z-index: 10;
+	}
 }
 
 .child {
 	width: 150px;
 	height: 150px;
+	background: #fff;
 	position: absolute;
 	top: 0;
 	left: 0;
-	background: #ccc;
-	z-index: -2;
+
+	.active & {
+		position: static;
+	}
+
 }
 </style>
