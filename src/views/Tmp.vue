e2f05a11e13a2f9259b99f6cf5349f4327137c6e
Volkov Alexander <volkov.525@gmail.com>
2024-12-20 18:19:20 +0300

    tmp works fine

--- src/views/Tmp.vue
+++ src/views/Tmp.vue
@@ -5,69 +5,71 @@ import { Flip } from 'gsap/Flip'
 
 gsap.registerPlugin(Flip)
 
-const expand = () => {
-	console.log(111)
-	const state = Flip.getState('.it, .ani')
-	expanded.value = !expanded.value
-	nextTick(() => {
-		Flip.from(state, {
-			duration: .6,
-			targets: '.it, .ani',
-			absolute: true,
-			absoluteOnLeave: true,
-			ease: "elastic.out(.5, 0.9)",
-		})
-	})
-}
+// const expand = () => {
+// 	console.log(111)
+// 	const state = Flip.getState('.it, .ani')
+// 	expanded.value = !expanded.value
+// 	nextTick(() => {
+// 		Flip.from(state, {
+// 			duration: .6,
+// 			targets: '.it, .ani',
+// 			absolute: true,
+// 			absoluteOnLeave: true,
+// 			ease: "elastic.out(.5, 0.9)",
+// 		})
+// 	})
+// }
 const expanded = ref(false)
 const expanded1 = ref(false)
 
 const calcClass = (item: any) => {
 	if (expanded1.value == true && item.expand == true) return 'act'
-	if (expanded1.value == true && item.expand == false) return 'inact'
+	// if (expanded1.value == true && item.expand == false) return 'inact'
 	else return ''
 }
 
-const expand1 = (item: any) => {
-	const state = Flip.getState('.child')
-	expanded1.value = !expanded1.value
+const expand = (item: any) => {
+	console.log(item)
+	const state = Flip.getState('.it, .dialog')
 	item.expand = !item.expand
+	// expanded.value = !expanded.value
 	nextTick(() => {
 		Flip.from(state, {
 			duration: .4,
-			targets: '.child',
+			// targets: '.it, .dialog',
 			absolute: true,
-			absoluteOnLeave: true,
-			ease: "elastic.out(.8, 1)",
+			// absoluteOnLeave: true,
+			ease: "elastic.out(.8, 4)",
+			// fade: true
 		})
-		console.log(333)
 	})
 }
 
 
-const arr = [
+const arr = ref([
 	{ id: 0, expand: false },
 	{ id: 1, expand: false },
 	{ id: 2, expand: false },
-]
+])
+
+// const calcId = ((item) => {
+// 	return 'item' + item.id
+// })
 </script>
 
 <template lang="pug">
 q-page(padding)
 	.list
-		.it
-		.it(@click="expand" :class="{ active: expanded }")
-			.zag(v-if='!expanded1') Группа
-			.some(v-if='expanded')
-				.child(
-					v-for="(el, index) in arr" :key='el.id'
-					@click.stop="expand1(el)"
-					:class="calcClass(el)"
-					v-motion
-					:initial="{ scale: .3, opacity: 0 }"
-					:enter="{ scale: 1, opacity: 1, transition: { delay: 200 + 100 * index } }"
-					)
-		.it
+		template(v-for="item in arr" :key='item.id' )
+			.it(@click="expand(item)" :class="{ active: item.expand }" :data-flip-id='item.id') {{ item.id }}
+			.backdrop(v-if='item.expand'
+				data-flip-id='fuck'
+				v-motion
+				:initial="{ opacity: 0 }"
+				:enter='{ opacity: 1, }'
+				:duration='100'
+				)
+			.dialog(:class='{ active: item.expand }' :data-flip-id='item.id' @click='expand(item)')
 </template>
 
 <style scoped lang="scss">
@@ -83,51 +85,32 @@ q-page(padding)
 	position: relative;
 
 	&.active {
-		position: fixed;
-		height: 100vh;
-		width: 100vw;
-		left: 0;
-		right: 0;
-		top: 50px;
-		right: 0;
-		bottom: 0;
-		background-color: rgba(0, 80, 80, 0.15);
-		backdrop-filter: blur(3px);
-		// padding: 1rem;
-		// padding-top: 100px;
-		z-index: 10;
-
-		.some {
-			display: flex;
-			justify-content: center;
-			gap: 1rem;
-		}
+		visibility: hidden;
+		// display: none;
 	}
 }
 
-.child {
-	width: 150px;
-	height: 150px;
-	background: #fff;
 
-	&.act {
-		position: fixed;
-		height: 70vh;
-		width: 900px;
-		margin: 0 auto;
-		left: 60px;
-		right: 0;
-		margin-top: 53px;
-		border: 1px solid #ccc;
-		box-shadow: 2px 2px 6px rgba($color: #000000, $alpha: 0.2);
+.dialog {
+	background: $secondary;
+	position: fixed;
+	height: 70vh;
+	width: 900px;
+	margin: 0 auto;
+	left: 60px;
+	right: 0;
+	margin-top: 53px;
+	border-radius: .5rem;
+	// box-shadow: 2px 2px 6px rgba($color: #000000, $alpha: 0.2);
+	display: none;
+	z-index: 15;
 
+	&.active {
+		display: block;
 	}
-
-	&.inact {
-		display: none;
-	}
-
 }
+
+
 
 .zag {
 	font-size: 1rem;
@@ -138,5 +121,18 @@ q-page(padding)
 		margin-top: 10px;
 		margin-bottom: 1rem;
 	}
+}
+
+.backdrop {
+	width: 100vw;
+	height: 100vh;
+	background-color: rgba(0, 0, 0, 0);
+	backdrop-filter: blur(4px);
+	position: fixed;
+	left: 0;
+	right: 0;
+	top: 0;
+	bottom: 0;
+	z-index: 15;
 }
 </style>
