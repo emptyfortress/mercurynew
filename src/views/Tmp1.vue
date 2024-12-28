0c9cfa88d31a59fa52edc68ca09935ee3f3fc0ea
Volkov Alexander <volkov.525@gmail.com>
2024-12-28 14:28:04 +0300

    drag work page bug

--- src/views/Tmp1.vue
+++ src/views/Tmp1.vue
@@ -2,37 +2,14 @@
 import { animations, insert } from "@formkit/drag-and-drop"
 import { useDragAndDrop } from "@formkit/drag-and-drop/vue"
 
-const insertPointClasses = [
-	"absolute",
-	"bg-blue-500",
-	"z-[1000]",
-	"rounded-full",
-	"duration-[5ms]",
-	"before:block",
-	'before:content-["Insert"]',
-	"before:whitespace-nowrap",
-	"before:block",
-	"before:bg-blue-500",
-	"before:py-1",
-	"before:px-2",
-	"before:rounded-full",
-	"before:text-xs",
-	"before:absolute",
-	"before:top-1/2",
-	"before:left-1/2",
-	"before:-translate-y-1/2",
-	"before:-translate-x-1/2",
-	"before:text-white",
-	"before:text-xs",
-]
 
 const [parent, tapes] = useDragAndDrop([
-	"Depeche Mode",
-	"Duran Duran",
-	"Pet Shop Boys",
-	"Kraftwerk",
-	"Tears for Fears",
-	"Spandau Ballet",
+	{ id: 0, expand: false, label: "Depeche Mode", },
+	{ id: 1, expand: false, label: "Duran Duran", },
+	{ id: 2, expand: false, label: "Pet Shop Boys", },
+	{ id: 3, expand: false, label: "Kraftwerk", },
+	{ id: 4, expand: false, label: "Tears for Fears", },
+	{ id: 5, expand: false, label: "Spandau Ballet", },
 ],
 	{
 		plugins: [animations(),
@@ -48,31 +25,62 @@ const [parent, tapes] = useDragAndDrop([
 	},
 )
 
+const calcClass = ((item: any) => {
+	item.expand ? 'active' : ''
+
+})
+
+const expand = ((item: any) => {
+	// item.expand = !item.expand
+	console.log(item)
+})
+
 </script>
 
 <template lang="pug">
 q-page(padding)
-	h2 lakjdlk
-	.par(ref="parent")
-		.chil(v-for=" tape in tapes" :key="tape") {{ tape }}
+	.pa(ref="parent")
+		.chil(v-for=" (tape, index) in tapes" :key="tape.id"
+			)
+			.con {{ tape.label }}
 
 </template>
 
 <style scoped lang="scss">
-.par {
+.pa {
 	display: grid;
 	grid-template-columns: repeat(3, 150px);
 	column-gap: 1rem;
 	row-gap: 1rem;
+	margin: 0 auto;
+	width: 470px;
+	background: #ccc;
 }
 
 .chil {
 	height: 100px;
 	background: #fff;
 	padding: 1rem;
+
+	&.active {
+		position: fixed;
+		height: 70vh;
+		width: 900px;
+		margin: 0 auto;
+		top: 150px;
+		left: 0;
+		right: 0;
+		border: 1px solid #ccc;
+		box-shadow: 2px 2px 6px rgba($color: #000000, $alpha: 0.2);
+	}
 }
 
 .custom {
-	background: red;
+	background: hsl(213 38% 81% / 1);
+
+	.con {
+		display: none;
+	}
+
 }
 </style>
