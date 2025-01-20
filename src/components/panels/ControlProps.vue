9adbcfbc82d0f0afd6d5fdc7dd290d6350498537
Volkov Alexander <volkov.525@gmail.com>
2025-01-20 15:05:14 +0300

    fix anim

--- src/components/panels/ControlProps.vue
+++ src/components/panels/ControlProps.vue
@@ -12,24 +12,12 @@ const control = useControl()
 <template lang="pug">
 div
 	StringProp(v-if='control.selectedControl?.type == 0'
-		v-motion
-		:initial='{ opacity: 0, y: 20 }'
-		:enter='{ opacity: 1, y: 0 }'
 		)
 	TextProp(v-if='control.selectedControl?.type == 1'
-		v-motion
-		:initial='{ opacity: 0, y: 20 }'
-		:enter='{ opacity: 1, y: 0 }'
 		)
 	DateProp(v-if='control.selectedControl?.type == 2'
-		v-motion
-		:initial='{ opacity: 0, y: 20 }'
-		:enter='{ opacity: 1, y: 0 }'
 		)
 	ManProp(v-if='control.selectedControl?.type == 5'
-		v-motion
-		:initial='{ opacity: 0, y: 20 }'
-		:enter='{ opacity: 1, y: 0 }'
 		)
 </template>
 
