d47c056e274b6b167357e280668a157d6052954e
Volkov Alexander <volkov.525@gmail.com>
2025-01-24 17:40:29 +0300

    add app preview icons

--- src/components/AppPreview.vue
+++ src/components/AppPreview.vue
@@ -2,6 +2,9 @@
 import { useApps } from '@/stores/apps'
 import { useRouter } from 'vue-router'
 import { useStorage } from '@vueuse/core'
+import IconWizard from '@/components/icons/IconWizard.vue'
+import IconFlag from '@/components/icons/IconFlag.vue'
+import IconEntrance from '@/components/icons/IconEntrance.vue'
 
 const myapps = useApps()
 const router = useRouter()
@@ -35,13 +38,29 @@ div
 	div() Версия: {{ props.item.version }}
 
 	.myrow
-		.bt(@click='navigate') Выполнить первичные настройки
-		.bt(@click='navigate1') Перейти к приложению
+		.bt(@click='navigate')
+			div
+				IconFlag.ic
+				div Создать первичные настройки
+		.bt(@click='navigate')
+			div
+				IconWizard.ic
+				div Воспользоваться ИИ-помощником
+		.bt(@click='navigate1')
+			div
+				IconEntrance.ic
+				div Перейти к приложению
 
 </template>
 
 <style scoped lang="scss">
 .myrow {
+	// display: grid;
+	// grid-template-columns: repeat(3, auto);
+	// justify-items: center;
+	// align-items: stretch;
+	// column-gap: 1rem;
+	// row-gap: .5rem;
 	display: flex;
 	justify-content: center;
 	align-items: center;
@@ -51,14 +70,15 @@ div
 
 .bt {
 	width: 200px;
-	height: 100px;
+	height: 120px;
+	padding: 1rem;
 	border: 1px solid $secondary;
 	// background: $secondary;
 	border-radius: var(--rad);
 	text-align: center;
 	display: flex;
 	justify-content: center;
-	align-items: center;
+	align-items: start;
 	color: $secondary;
 	font-weight: 600;
 
@@ -66,5 +86,9 @@ div
 		background: $secondary;
 		color: white;
 	}
+}
+
+.ic {
+	font-size: 2rem;
 }
 </style>
