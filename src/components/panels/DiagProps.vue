f2ac9b24dc7c207a73fcf40b466e89a576a36de4
Volkov Alexander <volkov.525@gmail.com>
2025-06-23 12:31:53 +0300

    remove empty

--- src/components/panels/DiagProps.vue
+++ src/components/panels/DiagProps.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
-import etapProps from '@/assets/img/etap.png'
-import roleProps from '@/assets/img/role.png'
+// import etapProps from '@/assets/img/etap.png'
+// import roleProps from '@/assets/img/role.png'
 import { useDiagram } from '@/stores/diagram'
 import IconAttribute from '@/components/icons/IconAttribute.vue'
 import IconRelation from '@/components/icons/IconRelation.vue'
-import Empty1 from '@/components/Empty1.vue'
+// import Empty1 from '@/components/Empty1.vue'
 import PropField from '@/components/common/PropField.vue'
 
 const mydiagram = useDiagram()
@@ -61,50 +61,51 @@ const list = [
 </script>
 
 <template lang="pug">
-Empty1(v-if='!mydiagram.selection')
+div
+	// Empty1(v-if='!mydiagram.selection')
 
-template(v-if='mydiagram.selection && showRoot')
-	.text-h6 Процесс: Заявление на отпуск 
-	.q-mt-md.text-bold Длительность: 63ч.
+	template(v-if='mydiagram.selection && showRoot')
+		.text-h6 Процесс: Заявление на отпуск 
+		.q-mt-md.text-bold Длительность: 63ч.
 
-template(v-if='mydiagram.selection && showRole')
-	.text-h6 Роль: название роли
-	.q-mt-md.text-bold Правила выбора
+	template(v-if='mydiagram.selection && showRole')
+		.text-h6 Роль: название роли
+		.q-mt-md.text-bold Правила выбора
 
-q-list(v-if='mydiagram.selection && showEtap' separator)
-	q-expansion-item(v-model="item1")
-		template(v-slot:header)
-			q-item-section(side)
-				IconAttribute.ic
-			q-item-section.zg Атрибуты
+	q-list(v-if='mydiagram.selection && showEtap' separator)
+		q-expansion-item(v-model="item1")
+			template(v-slot:header)
+				q-item-section(side)
+					IconAttribute.ic
+				q-item-section.zg Атрибуты
 
-		q-list.prop
-			PropField(v-for="item in list" :key='item.id'
-				:label='item.label'
-				v-model:main="item.main"
-				)
+			q-list.prop
+				PropField(v-for="item in list" :key='item.id'
+					:label='item.label'
+					v-model:main="item.main"
+					)
 
 
-	q-expansion-item(v-model="item2")
-		template(v-slot:header)
-			q-item-section(side)
-				IconRelation.ic
-			q-item-section.zg Дополнительно
+		q-expansion-item(v-model="item2")
+			template(v-slot:header)
+				q-item-section(side)
+					IconRelation.ic
+				q-item-section.zg Дополнительно
 
-// div
-// 	template(v-if='showRoot')
-// 		.text-h6 Процесс: Заявление на отпуск 
-// 		.q-mt-md.text-bold Длительность: 63ч.
+	// div
+	// 	template(v-if='showRoot')
+	// 		.text-h6 Процесс: Заявление на отпуск 
+	// 		.q-mt-md.text-bold Длительность: 63ч.
 
-	// template(v-if='showEtap')
-	// 	img(:src="etapProps" usemap="#image-map" width="335")
-	// 	map(name="image-map")
-	// 		area(target="_self" alt="next" title="next" @click="next" coords="299,154,346,197" shape="rect")
+		// template(v-if='showEtap')
+		// 	img(:src="etapProps" usemap="#image-map" width="335")
+		// 	map(name="image-map")
+		// 		area(target="_self" alt="next" title="next" @click="next" coords="299,154,346,197" shape="rect")
 
-	// template(v-if='showRole')
-	// 	img(:src="roleProps" usemap="#image-map" width="335")
-	// 	map(name="image-map")
-	// 		area(target="_self" alt="next" title="next" @click="next" coords="275,209,330,248" shape="rect")
+		// template(v-if='showRole')
+		// 	img(:src="roleProps" usemap="#image-map" width="335")
+		// 	map(name="image-map")
+		// 		area(target="_self" alt="next" title="next" @click="next" coords="275,209,330,248" shape="rect")
 
 </template>
 
