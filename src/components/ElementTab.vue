931a88e3c018f29ce913169e77e58ded073883c6
Volkov Alexander <volkov.525@gmail.com>
2025-06-25 13:21:55 +0300

    confirm dialog on delete field

--- src/components/ElementTab.vue
+++ src/components/ElementTab.vue
@@ -4,17 +4,45 @@ import { useDragAndDrop } from '@formkit/drag-and-drop/vue'
 import AddFormButton from '@/components/common/AddFormButton.vue'
 import { state } from '@formkit/drag-and-drop'
 import { Kind } from '@/types/enum'
+import IconText from '@/components/icons/IconText.vue'
+import MdiCalendar from '@/components/icons/MdiCalendar.vue'
+import MaterialSymbolsAccountCircle from '@/components/icons/MaterialSymbolsAccountCircle.vue'
+import DeleteDialog from '@/components/DeleteDialog.vue'
 
-interface Field {
-	id: number
-	label: string
-	caption: string
-	selected: boolean
-	type: Kind.Man
-	pic: any
-}
-
-const elements = ref<Field[]>([])
+const elements = ref([
+	{
+		id: 0,
+		label: 'Автор',
+		caption: 'Кто создал заявку',
+		selected: false,
+		type: Kind.Man,
+		pic: MaterialSymbolsAccountCircle,
+	},
+	{
+		id: 1,
+		label: 'Дата начала отпуска',
+		caption: 'Планируемая дата старта',
+		selected: false,
+		type: Kind.Date,
+		pic: MdiCalendar,
+	},
+	{
+		id: 2,
+		label: 'Дата окончания отпуска',
+		caption: 'Планируемая дата завершения',
+		selected: false,
+		type: Kind.String,
+		pic: MdiCalendar,
+	},
+	{
+		id: 3,
+		label: 'Комментарий',
+		caption: 'Свободный комментарий',
+		selected: false,
+		type: Kind.Text,
+		pic: IconText,
+	},
+])
 
 const [lib, libitems] = useDragAndDrop(elements.value, { sortable: false, group: 'one' })
 
@@ -32,8 +60,17 @@ const create = (e: Field) => {
 	libitems.value.push(e)
 }
 
-const remove = (ind: number) => {
-	libitems.value.splice(ind, 1)
+const remove = () => {
+	libitems.value.splice(removedIndex.value, 1)
+}
+const dialog = ref(false)
+const removedItem = ref('')
+const removedIndex = ref(0)
+
+const confirm = (e: string, num: number) => {
+	removedItem.value = e
+	removedIndex.value = num
+	dialog.value = true
 }
 </script>
 
@@ -49,14 +86,17 @@ q-list.list(v-else bordered separator ref="lib")
 			q-item-label.grey(caption) {{ item.caption }}
 
 		q-item-section(side)
-			q-btn.remove(flat round icon="mdi-trash-can-outline" color="secondary" dense size="sm") 
-				q-menu
-					q-list(dense)
-						q-item.pink(clickable @click="remove(index)")
-							q-item-section Удалить
+			q-btn.remove(flat round icon="mdi-delete-outline" color="secondary" dense size="sm" @click='confirm(item.label, index)') 
+
+				// q-menu
+				// 	q-list(dense)
+				// 		q-item.pink(clickable @click="remove(index)")
+				// 			q-item-section Да, удалить!
 
 br
 AddFormButton(@create='create')
+DeleteDialog(v-model="dialog" :field='removedItem' @remove="remove")
+// Trash(:dragging='true')
 </template>
 
 <style scoped lang="scss">
