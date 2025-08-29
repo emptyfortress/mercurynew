19463ba4b0f9fc2bd388e11aa093381313281816
Volkov Alexander <volkov.525@gmail.com>
2025-08-29 13:09:29 +0300

    add roza to list

--- src/components/ShareDialog.vue
+++ src/components/ShareDialog.vue
@@ -30,20 +30,13 @@ const confirm = () => {
 
 <template lang="pug">
 q-dialog(v-model="modelValue")
-	q-card(style="width: 400px;")
+	q-card(style="width: 420px;")
 		q-btn.close(round color="negative" icon="mdi-close" v-close-popup)
 		q-card-section
 			.text-h6 Доступ к приложению
-			.q-mt-md Пользователи имеющие доступ к приложению:
+			.q-mt-md Пользователи, имеющие право редактирования:
 			br
 			q-list(bordered separator)
-				q-item(clickable)
-					q-item-section(side)
-						q-avatar(size='md')
-							img(src="https://cdn.quasar.dev/img/avatar.png")
-					q-item-section Орлов П.С.
-					q-item-section(side)
-						.caption Вы (автор)
 				q-item(clickable)
 					q-item-section(side)
 						q-icon(name="mdi-account-circle" color="primary" size='md')
@@ -51,14 +44,31 @@ q-dialog(v-model="modelValue")
 					q-item-section(side)
 						.caption Системная роль
 
-			.text-bold.q-mt-md Общий доступ
+				q-item(clickable)
+					q-item-section(side)
+						q-avatar(size='md')
+							img(src="https://cdn.quasar.dev/img/avatar.png")
+					q-item-section Орлов П.С.
+					q-item-section(side)
+						.caption Вы (автор)
+
+				q-item(clickable)
+					q-item-section(side)
+						q-avatar(size='28px' color="positive" text-color="white") РЛ
+					q-item-section
+							div Роза Львовна
+							.caption По приглашению
+					q-item-section(side)
+						q-icon(name="mdi-close-circle" color="grey" size="sm")
+
+			.text-bold.q-mt-md Доступ на редактирование
 			q-select(outlined dense v-model="dostup" :options='options')
 				template(v-slot:prepend)
 					q-icon(v-if='dostup == "Доступ ограничен"' name="mdi-lock" color="primary")
 					q-icon(v-else name="mdi-link" color="primary")
 			.hint
-				span(v-if='dostup =="Доступ ограничен"') Открывать приложение могут только пользователи, имеющие доступ.
-				span(v-else) Открывать приложение могут все, у кого есть ссылка.
+				span(v-if='dostup =="Доступ ограничен"') Редактировать приложение могут только пользователи, из списка выше.
+				span(v-else) Редактировать приложение могут все, у кого есть ссылка.
 		q-card-actions.q-mx-md(align='right')
 			q-btn(v-if='localMultiuser' outline rounded color="primary" icon="mdi-link" label="Копировать ссылку" size="sm") 
 			q-space
@@ -74,5 +84,9 @@ q-dialog(v-model="modelValue")
 	background: #eee;
 	padding: 0.5rem;
 	text-align: center;
+}
+.caption {
+	font-size: 0.7rem;
+	color: #666;
 }
 </style>
