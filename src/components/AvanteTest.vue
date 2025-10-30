c5685060638f3deadb3b579cf429e019a10aa84c
Volkov Alexander <volkov.525@gmail.com>
2025-10-30 14:25:36 +0300

    test

---
+++ src/components/AvanteTest.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const test = ref()
+</script>
+
+<template lang="pug">
+h2 Hello {{ test }}
+p This is a dummy paragraph. This is some more text for the first paragraph.
+p This is another dummy paragraph.
+</template>
+
+<style scoped lang="scss">
+h2 {
+	color: #ff9999;
+}
+</style>
