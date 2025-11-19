4cd3e2bbd24a3303b002b91c8b26cc664c5b65fc
Volkov Alexander <volkov.525@gmail.com>
2025-11-19 11:16:33 +0300

    clean

--- src/views/Process.vue
+++ src/views/Process.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch, onMounted } from 'vue'
+import { ref, watch } from 'vue'
 import PlusButton from '@/components/PlusButton.vue'
 import { usePanels } from '@/stores/panels'
 import DiagramSvg from '@/components/DiagramSvg.vue'
