8b69a24e34e4ae0fd867847383d509a08dbb83bc
Volkov Alexander <volkov.525@gmail.com>
2026-03-19 13:39:03 +0300

    add chips component

---
+++ src/components/decision/Chips.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref, reactive, watch } from 'vue'
+
+const roles = reactive([
+	{ selected: false, id: 'admin', label: 'Администратор' },
+	{ selected: false, id: 'manager', label: 'Автор' },
+	{ selected: false, id: 'executor', label: 'Исполнитель' },
+	{ selected: false, id: 'observer', label: 'Наблюдатель' },
+	{ selected: false, id: 'auditor', label: 'Аудитор' },
+	{ selected: false, id: 'developer', label: 'Разработчик' },
+	{ selected: false, id: 'analyst', label: 'Аналитик' },
+	{ selected: false, id: 'guest', label: 'Гость' },
+])
+</script>
+
+<template lang="pug">
+.chips-container
+	q-chip(
+		v-for='chip in roles'
+		:key='chip.id'
+		v-model:selected='chip.selected'
+	) {{ chip.label }}
+</template>
+
+<style scoped lang="scss">
+.chips-container {
+	display: flex;
+	flex-wrap: wrap;
+}
+:deep(.q-chip--selected) {
+	background: $primary;
+	color: white;
+	i {
+		color: white;
+	}
+}
+</style>
--- src/components/decision/Matrix.vue
+++ src/components/decision/Matrix.vue
@@ -1,14 +1,53 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import Chips from '@/components/decision/Chips.vue'
+
+const tab = ref<'role' | 'state' | 'operation'>('role')
+
+const isDirty = ref(true)
+
+const roles = [
+	{ id: 'admin', label: 'Администратор' },
+	{ id: 'manager', label: 'Менеджер' },
+	{ id: 'executor', label: 'Исполнитель' },
+	{ id: 'observer', label: 'Наблюдатель' },
+	{ id: 'auditor', label: 'Аудитор' },
+	{ id: 'developer', label: 'Разработчик' },
+	{ id: 'analyst', label: 'Аналитик' },
+	{ id: 'guest', label: 'Гость' },
+]
+</script>
 
 <template lang="pug">
 q-page(padding)
 	.container
-		.text-h6 Матрица доступа
+		.row.justify-between
+			.text-h6 Матрица доступа
+			.q-gutter-x-sm(v-if='isDirty')
+				q-btn(flat round icon="mdi-lock" color="primary") 
+					q-tooltip Блокировка активна
+				q-btn(@click="" flat color="primary" label="Отмена")
+				q-btn(@click="" color="primary" label="Сохранить")
+		
+		q-tabs(v-model='tab' class='q-mt-md' align='left')
+			q-tab(name='role' label='Роль')
+			q-tab(name='state' label='Состояние')
+			q-tab(name='operation' label='Операция')
+		
+		q-tab-panels(v-model='tab' animated)
+			q-tab-panel(name='role')
+				Chips(chip='roles')
+
+			q-tab-panel(name='state')
+				.text-body2 Контент вкладки Состояние
+
+			q-tab-panel(name='operation')
+				.text-body2 Контент вкладки Операция
 </template>
 
 <style scoped lang="scss">
 .container {
-	max-width: 1200px;
-	margin: 0 auto;
+	// max-width: 1200px;
+	margin: 0 2rem;
 }
 </style>
