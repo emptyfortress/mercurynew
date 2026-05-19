8e38247e903a881331cf4dc4efc130a9ba2f72a3
Volkov Alexander <volkov.525@gmail.com>
2026-05-19 11:36:48 +0300

    feat: add actions column with edit and delete options in Decisions view

--- src/views/Decisions.vue
+++ src/views/Decisions.vue
@@ -36,6 +36,7 @@ const tableColumns = [
 	{ name: 'name', label: 'Название', field: 'name', align: 'left' as const },
 	{ name: 'author', label: 'Автор', field: 'author', align: 'left' as const },
 	{ name: 'date', label: 'Дата', field: 'date', align: 'left' as const },
+	{ name: 'actions', label: '', field: 'actions', align: 'right' as const },
 ]
 
 const tableData = [
@@ -98,7 +99,7 @@ q-page(padding)
 					q-item-section {{ item.label }}
 
 			div
-				div(v-if='selectedListItem == null') alsjdlaksj
+				div(v-if='selectedListItem == null') Ничего не выбрано
 				div(v-if='selectedListItem == 0')
 					.text-bold Главное меню
 					div Выберите меню для настройки или создайте новое
@@ -108,6 +109,21 @@ q-page(padding)
 						row-key="name"
 						flat
 					)
+						template(v-slot:body-cell-actions='props')
+							q-td(:props="props")
+								q-btn.q-mr-md(flat round dense icon="mdi-pencil" size='sm') 
+								q-btn(flat round dense icon="mdi-dots-vertical" size='sm') 
+									q-menu
+										q-list
+											q-item(clickable)
+												q-item-section(side)
+													q-icon(name="mdi-content-duplicate" color="primary")
+												q-item-section Дублировать
+											q-item(clickable)
+												q-item-section(side)
+													q-icon(name="mdi-delete-outline" color="negative")
+												q-item-section Удалить
+
 				div(v-if='selectedListItem && selectedListItem > 0')
 					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE --><path fill="currentColor" d="m18.9 21l-5.475-5.475l2.1-2.1L21 18.9zM5.1 21L3 18.9L9.9 12l-1.7-1.7l-.7.7l-1.275-1.275v2.05l-.7.7L2.5 9.45l.7-.7h2.05L4 7.5l3.55-3.55q.5-.5 1.075-.725T9.8 3t1.175.225t1.075.725l-2.3 2.3L11 7.5l-.7.7L12 9.9l2.25-2.25q-.1-.275-.162-.575t-.063-.6q0-1.475 1.013-2.488t2.487-1.012q.375 0 .713.075t.687.225L16.45 5.75l1.8 1.8l2.475-2.475q.175.35.238.687t.062.713q0 1.475-1.012 2.488t-2.488 1.012q-.3 0-.6-.05t-.575-.175z"/></svg>
 					div Раздел в разработке
