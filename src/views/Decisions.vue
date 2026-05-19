b3c6b2a280abfd37c925be9a9243737ebee19e22
Volkov Alexander <volkov.525@gmail.com>
2026-05-19 13:37:25 +0300

    add row selection

--- src/views/Decisions.vue
+++ src/views/Decisions.vue
@@ -45,9 +45,9 @@ const tableData = ref([
 	{ uid: uid(), name: 'КЭДО меню', author: 'admin', date: '2024-01-20' },
 ])
 
-const selectedRow = ref<{ uid: string; name: string; author: string; date: string } | null>(null)
+const selectedRow = ref()
 
-const selectRow = (row: { uid: string; name: string; author: string; date: string }) => {
+const selectRow = (row: any) => {
 	selectedRow.value = row
 }
 
@@ -75,14 +75,23 @@ const create = () => {
 
 const createMenu = () => {
 	if (newMenuName.value.trim()) {
-		tableData.value.push({
+		let tmp = {
 			uid: uid(),
 			name: newMenuName.value.trim(),
 			author: 'admin',
 			date: new Date().toISOString().split('T')[0],
-		})
+		}
+		tableData.value.push(tmp)
 		createDialog.value = false
+		selectedRow.value = tmp
 	}
+}
+
+const calcClass = (uid: string) => {
+	if (selectedRow.value && selectedRow.value.uid == uid) {
+		return 'selectedtr'
+	}
+	return ''
 }
 </script>
 
@@ -152,31 +161,32 @@ q-page(padding)
 							@click="create",
 							size='sm'
 						) 
-			
 					q-table.q-mt-md(
 						:rows="tableData"
 						:columns="tableColumns"
 						row-key="uid"
 						flat
 						hide-bottom
-						:selected="selectedRow"
-						selection="single"
-						@update:selected="selectRow"
 					)
-						template(v-slot:body-cell-actions='props')
-							q-td(:props="props")
-								q-btn.q-mr-md(flat round dense icon="mdi-pencil" size='sm') 
-								q-btn(flat round dense icon="mdi-dots-vertical" size='sm') 
-									q-menu
-										q-list
-											q-item(clickable @click="duplicateTableItem(props.row)" v-close-popup)
-												q-item-section(side)
-													q-icon(name="mdi-content-duplicate" color="primary")
-												q-item-section Дублировать
-											q-item(clickable @click="deleteTableItem(props.row.uid)" v-close-popup)
-												q-item-section(side)
-													q-icon(name="mdi-delete-outline" color="negative")
-												q-item-section Удалить
+						template(v-slot:body='props')
+							q-tr(:props="props" @click="selectRow(props.row)" :class="calcClass(props.row.uid)")
+								q-td(:props='props' key="name") {{ props.row.name }}
+								q-td(:props='props' key="author") {{ props.row.author }}
+								q-td(:props='props' key="date") {{ props.row.date }}
+
+								q-td(:props="props" key='actions')
+									q-btn.q-mr-md(flat round dense icon="mdi-pencil" size='sm') 
+									q-btn(flat round dense icon="mdi-dots-vertical" size='sm') 
+										q-menu
+											q-list
+												q-item(clickable @click="duplicateTableItem(props.row)" v-close-popup)
+													q-item-section(side)
+														q-icon(name="mdi-content-duplicate" color="primary")
+													q-item-section Дублировать
+												q-item(clickable @click="deleteTableItem(props.row.uid)" v-close-popup)
+													q-item-section(side)
+														q-icon(name="mdi-delete-outline" color="negative")
+													q-item-section Удалить
 
 				div(v-if='selectedListItem && selectedListItem > 0')
 					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE --><path fill="currentColor" d="m18.9 21l-5.475-5.475l2.1-2.1L21 18.9zM5.1 21L3 18.9L9.9 12l-1.7-1.7l-.7.7l-1.275-1.275v2.05l-.7.7L2.5 9.45l.7-.7h2.05L4 7.5l3.55-3.55q.5-.5 1.075-.725T9.8 3t1.175.225t1.075.725l-2.3 2.3L11 7.5l-.7.7L12 9.9l2.25-2.25q-.1-.275-.162-.575t-.063-.6q0-1.475 1.013-2.488t2.487-1.012q.375 0 .713.075t.687.225L16.45 5.75l1.8 1.8l2.475-2.475q.175.35.238.687t.062.713q0 1.475-1.012 2.488t-2.488 1.012q-.3 0-.6-.05t-.575-.175z"/></svg>
@@ -214,7 +224,7 @@ q-page(padding)
 	color: var(--dark);
 }
 .selected {
-	background: var(--selected);
+	background: var(--selection);
 }
 .container {
 	max-width: 1400px;
@@ -244,5 +254,8 @@ q-page(padding)
 .page-leave-to {
 	opacity: 0;
 	transform: translateY(-10px);
+}
+.selectedtr {
+	background: var(--selection);
 }
 </style>
