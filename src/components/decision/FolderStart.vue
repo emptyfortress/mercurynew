678959f2e581b2d0293bc1d7eed11b66fa1d5aac
Volkov Alexander <volkov.525@gmail.com>
2026-05-20 13:39:49 +0300

    dialog added

--- src/components/decision/FolderStart.vue
+++ src/components/decision/FolderStart.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, nextTick } from 'vue'
 import { useSimpleStore } from '@/stores/simpleStore'
 import { storeToRefs } from 'pinia'
 import ChooseCardTree from '@/components/decision/ChooseCardTree.vue'
 import ChooseCardView from '@/components/decision/ChooseCardView.vue'
 import ChooseTemplate from '@/components/decision/ChooseTemplate.vue'
 import Safety from '@/components/decision/Safety.vue'
+import { BaseTree } from '@he-tree/vue'
+import '@he-tree/vue/style/default.css'
 
 const simpleStore = useSimpleStore()
 const { selectedElement } = storeToRefs(simpleStore)
@@ -30,16 +32,7 @@ const saveChanges = () => {
 }
 
 const view = ref('дайджест')
-const link = ref('')
-const card = ref('Карточка 1')
 const optimize = ref(true)
-const show = ref(1)
-const showOption = [
-	{ value: 1, label: 'Папку' },
-	{ value: 2, label: 'Карточку' },
-	{ value: 3, label: 'URL' },
-]
-const renew = ref(true)
 const autorenew = ref(1)
 const autoOption = [
 	{ value: 1, label: 'Выключено' },
@@ -48,7 +41,6 @@ const autoOption = [
 ]
 const quan = ref(40)
 const unread = ref(true)
-const first = ref(true)
 const access = ref(1)
 const accessOption = [
 	{ value: 1, label: 'Все карточки' },
@@ -60,6 +52,88 @@ const folderType = ref('Стандартная')
 const searchZapros = ref('Запрос 1')
 
 const dialog = ref(false)
+
+const tree = [
+	{
+		id: 1,
+		text: 'Договоры',
+		children: [
+			{
+				id: 2,
+				text: 'Договоры за 2025 год',
+			},
+			{
+				id: 3,
+				text: 'Договоры с истекающим сроком',
+			},
+			{
+				id: 4,
+				text: 'Не подписанные договоры',
+			},
+		],
+	},
+	{
+		id: 5,
+		text: 'Счета и акты',
+		children: [
+			{
+				id: 6,
+				text: 'Неоплаченные счета',
+			},
+			{
+				id: 7,
+				text: 'Акты без подписи',
+			},
+			{
+				id: 8,
+				text: 'Счета от поставщиков',
+			},
+		],
+	},
+	{
+		id: 9,
+		text: 'Кадровые документы',
+		children: [
+			{
+				id: 10,
+				text: 'Заявления на отпуск',
+			},
+			{
+				id: 11,
+				text: 'Приказы о приеме',
+			},
+			{
+				id: 12,
+				text: 'Командировочные документы',
+			},
+		],
+	},
+	{
+		id: 13,
+		text: 'Общие поиски',
+		children: [
+			{
+				id: 14,
+				text: 'Документы без категории',
+			},
+			{
+				id: 15,
+				text: 'Недавно измененные документы',
+			},
+		],
+	},
+]
+const select = (stat: any) => {
+	if (stat.children.length) {
+		stat.open = !stat.open
+	} else {
+		zapr.value.statsFlat.map((el: any) => (el.checked = false))
+		nextTick(() => {
+			stat.checked = !stat.checked
+		})
+	}
+}
+const zapr = ref()
 </script>
 
 <template lang="pug">
@@ -85,7 +159,7 @@ const dialog = ref(false)
 
 				template(v-if='simpleStore.selectedElement?.virtual')
 					.label Поисковый запрос
-				.flex.items-center.q-gutter-x-sm
+					.flex.items-center.q-gutter-x-sm
 						q-select(dense v-model="searchZapros" outlined :options='["Запрос 1", "Запрос 2", "Запрос 3", "Запрос 4", "Запрос 5"]')
 						q-btn(outline dense icon="mdi-dots-horizontal" color="primary" @click="dialog = !dialog") 
 	
@@ -125,14 +199,33 @@ const dialog = ref(false)
 		q-btn(flat color="primary" label="Отмена" @click="cancelChanges") 
 		q-btn(unelevated color="primary" label="Сохранить" @click="saveChanges") 
 
-q-dialog(v-model="dialog" backdrop-filter="blur(4px) saturate(150%)")
-	q-card(style="min-width: 400px;")
-		q-btn.close(round color="negative" icon="mdi-close" v-close-popup)
-		q-card-section
-			.text-h6 Поисковые запросы
-			.caption Выберите нужный запрос из дерева
 
-		q-card-section test
+	q-dialog(v-model="dialog" backdrop-filter="blur(4px) saturate(150%)")
+		q-card(style="min-width: 400px; min-height: 600px")
+			q-btn.close(round color="negative" icon="mdi-close" v-close-popup)
+			q-card-section
+				.text-h6 Поисковые запросы
+				.caption Выберите нужный запрос из дерева
+
+			q-card-section
+				BaseTree(v-model="tree"
+					ref="zapr"
+					propKey="id"
+					treeLine
+					:treeLineOffset="18"
+					:indent="30"
+					:defaultOpen='true'
+					)
+					template(#default="{ node, stat }")
+						.node(@click.stop="select(stat)" :class="{selected : stat.checked}")
+							q-icon(name="mdi-chevron-down" v-if="stat.children.length" :class="{ 'closed': !stat.open }").trig
+							q-icon(name="mdi-folder-outline" v-if="stat.children.length").fold
+							input.q-mr-sm(v-if='!stat.children.length' type="checkbox" v-model="stat.checked")
+							span {{ node.text }}
+
+			q-card-actions(align='right')
+				q-btn(flat color="primary" label="Отмена" v-close-popup) 
+				q-btn(unelevated color="primary" label="Сохранить" v-close-popup) 
 </template>
 
 <style scoped lang="scss">
@@ -156,5 +249,37 @@ q-dialog(v-model="dialog" backdrop-filter="blur(4px) saturate(150%)")
 :deep(.q-tab-panels) {
 	background: transparent;
 	min-height: 400px;
+}
+.node {
+	padding: 4px 8px;
+	cursor: pointer;
+
+	&.selected {
+		background: var(--selection);
+		color: var(--dark2);
+
+		&:hover {
+			background: var(--selection);
+			color: black;
+		}
+	}
+
+	&:hover {
+		background: #edf0f8;
+	}
+}
+.fold {
+	font-size: 1.3rem;
+	margin-right: 0.5rem;
+}
+
+.trig {
+	font-size: 1.3rem;
+	margin-right: 0.5rem;
+	transition: 0.2s ease all;
+
+	&.closed {
+		transform: rotate(-90deg);
+	}
 }
 </style>
